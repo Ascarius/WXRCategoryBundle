7596dc3ae21f129abf2548981f0db7ffc204d1d9
Lionel Gaillard <lionel@wxrstudios.com>
2012-10-11 15:30:22 +0200

    Added Entity\CategoryAdmin

---
+++ src/WXR/CategoryBundle/Admin/Entity/CategoryAdmin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WXR\CategoryBundle\Admin\Entity;
+
+use WXR\CategoryBundle\Admin\Model\CategoryAdmin as BaseCategoryAdmin;
+
+abstract class CategoryAdmin extends BaseCategoryAdmin
+{
+}
