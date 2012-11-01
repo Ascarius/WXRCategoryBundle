7969e183e5e9fc26e6aba7fcdaefeb2ed2f1bfd4
Lionel Gaillard <lionel@wxrstudios.com>
2012-11-01 22:53:46 +0100

    Again...

--- src/WXR/CategoryBundle/Model/Category.php
+++ src/WXR/CategoryBundle/Model/Category.php
@@ -21,7 +21,7 @@ abstract class Category extends Tag implements CategoryInterface
     public function __construct()
     {
         parent::__construct();
-        $position = 0;
+        $this->position = 0;
     }
 
     /**
